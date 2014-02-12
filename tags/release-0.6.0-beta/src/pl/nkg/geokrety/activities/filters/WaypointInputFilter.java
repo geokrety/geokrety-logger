da0365ffc62a761ed1459ce469cceb3e7e661b36
dzwiedziu-nkg <dzwiedziu-nkg@ff536b33-f402-454f-b109-af6ef765b36a>
2014-02-12 17:12:55 +0000

    Tagging the 0.6.0 beta release.
    git-svn-id: https://svn.code.sf.net/p/geokretylog/code@192 ff536b33-f402-454f-b109-af6ef765b36a

---
+++ tags/release-0.6.0-beta/src/pl/nkg/geokrety/activities/filters/WaypointInputFilter.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2014 Michał Niedźwiecki
+ * 
+ * This file is a part of GeoKrety Logger
+ * http://geokretylog.sourceforge.net/
+ * 
+ * GeoKrety Logger is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this source code; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * or see <http://www.gnu.org/licenses/>
+ */
+package pl.nkg.geokrety.activities.filters;
+
+import java.util.regex.Pattern;
+
+public class WaypointInputFilter extends RegExInputFilter {
+
+    public static final Pattern LETTERS_AND_DIGITS_PATTERN = Pattern.compile("[a-zA-Z0-9]*");
+
+    public WaypointInputFilter() {
+        super(LETTERS_AND_DIGITS_PATTERN);
+    }
+    
+    @Override
+    public boolean validate(CharSequence text) {
+        // TODO: check min and max length of waypoints
+        return text.length() >= 6 && super.validate(text);
+    }
+}
---
+++ tags/release-0.6.0-beta/src/pl/nkg/geokrety/threads/GettingUuidThread.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2013, 2014 Michał Niedźwiecki
+ * 
+ * This file is a part of GeoKrety Logger
+ * http://geokretylog.sourceforge.net/
+ * 
+ * GeoKrety Logger is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this source code; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * or see <http://www.gnu.org/licenses/>
+ */
+package pl.nkg.geokrety.threads;
+
+import pl.nkg.geokrety.GeoKretyApplication;
+import pl.nkg.lib.okapi.OKAPIProvider;
+import pl.nkg.lib.okapi.SupportedOKAPI;
+import pl.nkg.lib.threads.AbstractForegroundTaskWrapper;
+import pl.nkg.lib.threads.ForegroundTaskHandler;
+import android.util.Pair;
+
+public class GettingUuidThread extends
+		AbstractForegroundTaskWrapper<Pair<String, Integer>, String, String> {
+
+	public static final int ID = 4;
+
+	public GettingUuidThread(GeoKretyApplication application) {
+		super(application, ID);
+	}
+
+	public static GettingUuidThread getFromHandler(ForegroundTaskHandler handler) {
+		AbstractForegroundTaskWrapper<?, ?, ?> a = handler.getTask(ID);
+		GettingUuidThread b = (GettingUuidThread) a;
+		return b;
+	}
+
+	@Override
+	protected String runInBackground(Thread thread, Pair<String, Integer> param)
+			throws Throwable {
+		return OKAPIProvider.loadOpenCachingUUID(
+				SupportedOKAPI.SUPPORTED[param.second], param.first);
+	}
+}
