b405f226380567953f528ed8feb2a43001813c67
dzwiedziu-nkg <dzwiedziu-nkg@ff536b33-f402-454f-b109-af6ef765b36a>
2014-02-12 08:07:33 +0000

    Tagging the 0.5.99.7 beta release.
    git-svn-id: https://svn.code.sf.net/p/geokretylog/code@190 ff536b33-f402-454f-b109-af6ef765b36a

---
+++ tags/release-0.5.99.7-beta/src/pl/nkg/geokrety/activities/AbstractGeoKretyActivity.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2014 Michał Niedźwiecki
+ * 
+ * This file is a part of GeoKrety Logger
+ * http://geokretylog.sourceforge.net/
+ * 
+ * GeoKrety Logger is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this source code; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * or see <http://www.gnu.org/licenses/>
+ */
+package pl.nkg.geokrety.activities;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import pl.nkg.geokrety.GeoKretyApplication;
+import pl.nkg.geokrety.R;
+import pl.nkg.geokrety.data.StateHolder;
+import pl.nkg.geokrety.services.LogSubmitterService;
+import pl.nkg.geokrety.services.RefreshService;
+import pl.nkg.lib.dialogs.ManagedDialogsActivity;
+
+public abstract class AbstractGeoKretyActivity extends ManagedDialogsActivity {
+
+    protected GeoKretyApplication application;
+    protected StateHolder stateHolder;
+    protected SQLiteDatabase database;
+    protected Cursor cursor;
+    private boolean useDataBase = false;
+    
+    private final BroadcastReceiver refreshFinishBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+            Toast.makeText(AbstractGeoKretyActivity.this, R.string.toast_notify_refresh_finish, Toast.LENGTH_LONG).show();
+            onRefreshDatabase();
+        }
+    };
+    
+    private final BroadcastReceiver refreshErrorBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+            String error = intent.getExtras().getString(RefreshService.INTENT_ERROR_MESSAGE);
+            Toast.makeText(AbstractGeoKretyActivity.this, getText(R.string.toast_notify_refresh_error) + " " + error, Toast.LENGTH_LONG).show();
+            onRefreshError();
+        }
+    };
+    
+    protected void onRefreshDatabase() {
+    }
+    
+    protected void onRefreshError() {
+    }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        application = (GeoKretyApplication)getApplication();
+        stateHolder = application.getStateHolder();
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(refreshErrorBroadcastReceiver);
+        unregisterReceiver(refreshFinishBroadcastReceiver);
+        if (useDataBase) {
+            database = null;
+            closeCursorIfOpened();
+            stateHolder.getDbHelper().closeDatabase();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (useDataBase) {
+            database = stateHolder.getDbHelper().openDatabase();
+            openCursor();
+        }
+        application.runRefreshService(false);
+        registerReceiver(refreshFinishBroadcastReceiver, new IntentFilter(
+                RefreshService.BROADCAST_FINISH));
+        registerReceiver(refreshErrorBroadcastReceiver, new IntentFilter(
+                RefreshService.BROADCAST_ERROR));
+    }
+    
+    protected Cursor openCursor() {
+        closeCursorIfOpened();
+        return null;
+    }
+
+    protected void closeCursorIfOpened() {
+        if (useDataBase == false) {
+            throw new RuntimeException("You forgot to use turnOnDatabaseUse() in onCreate()");
+        }
+        
+        if (cursor != null) {
+            cursor.close();
+            cursor = null;
+        }
+    }
+    
+    protected void turnOnDatabaseUse() {
+        if (useDataBase == false) {
+            useDataBase = true;
+        }
+    }
+}
---
+++ tags/release-0.5.99.7-beta/src/pl/nkg/geokrety/data/User.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (C) 2013, 2014 Michał Niedźwiecki
+ * 
+ * This file is a part of GeoKrety Logger
+ * http://geokretylog.sourceforge.net/
+ * 
+ * GeoKrety Logger is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this source code; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * or see <http://www.gnu.org/licenses/>
+ */
+
+package pl.nkg.geokrety.data;
+
+import pl.nkg.geokrety.Utils;
+import android.os.Bundle;
+
+public class User {
+    public static final String ACCOUNT_ID = "accountID";
+    public static final String ACCOUNT_NAME = "accountName";
+    public static final String SECID = "secid";
+    public static final String OCUUIDS = "ocUUIDs";
+    public static final String OCLOGINS = "ocLogins";
+    public static final String HOME_LON = "homeCordLon";
+    public static final String HOME_LAT = "homeCordLat";
+
+    private long id;
+    private String name;
+
+    private String geoKretySecredID;
+    private String[] openCachingUUIDs;
+    private String[] openCachingLogins;
+
+    private String homeCordLon;
+    private String homeCordLat;
+
+    public User(final Bundle bundle) {
+        unpack(bundle);
+    }
+
+    public User(final int id, final String name, final String geoKretySecredID,
+            final String[] openCachingUUIDs, final String[] openCachingLogins) {
+        this.id = id;
+        this.name = name;
+        this.geoKretySecredID = geoKretySecredID;
+        this.openCachingUUIDs = openCachingUUIDs;
+        this.openCachingLogins = openCachingLogins;
+    }
+
+    public String getGeoKreySecredID() {
+        return geoKretySecredID;
+    }
+
+    public String getHomeCordLat() {
+        return homeCordLat;
+    }
+
+    public String getHomeCordLon() {
+        return homeCordLon;
+    }
+
+    public long getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getOpenCachingLogins() {
+        return openCachingLogins;
+    }
+
+    public String[] getOpenCachingUUIDs() {
+        return openCachingUUIDs;
+    }
+
+    public boolean hasOpenCachingUUID(final int portal) {
+        if (openCachingUUIDs == null || portal < 0 || portal >= openCachingUUIDs.length) {
+            return false;
+        }
+
+        return !Utils.isEmpty(openCachingUUIDs[portal]);
+    }
+
+    public Bundle pack(final Bundle bundle) {
+        bundle.putStringArray(User.OCUUIDS, openCachingUUIDs);
+        bundle.putStringArray(User.OCLOGINS, openCachingLogins);
+        bundle.putLong(User.ACCOUNT_ID, id);
+        bundle.putString(User.SECID, geoKretySecredID);
+        bundle.putString(User.ACCOUNT_NAME, name);
+        bundle.putString(User.HOME_LAT, homeCordLat);
+        bundle.putString(User.HOME_LON, homeCordLon);
+        return bundle;
+    }
+
+    public void setHomeCordLat(final String homeCordLat) {
+        this.homeCordLat = homeCordLat;
+    }
+
+    public void setHomeCordLon(final String homeCordLon) {
+        this.homeCordLon = homeCordLon;
+    }
+
+    public void setID(final long id) {
+        this.id = id;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public Bundle unpack(final Bundle bundle) {
+        geoKretySecredID = bundle.getString(User.SECID);
+        id = bundle.getLong(User.ACCOUNT_ID);
+        openCachingUUIDs = bundle.getStringArray(User.OCUUIDS);
+        openCachingLogins = bundle.getStringArray(User.OCLOGINS);
+        name = bundle.getString(User.ACCOUNT_NAME);
+        homeCordLat = bundle.getString(User.HOME_LAT);
+        homeCordLon = bundle.getString(User.HOME_LON);
+        return bundle;
+    }
+}
