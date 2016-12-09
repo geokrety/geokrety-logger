f5b59bede187a239694d3a1d07bc7238fa20acc9
dzwiedziu-nkg <dzwiedziu-nkg@ff536b33-f402-454f-b109-af6ef765b36a>
2016-12-09 15:47:12 +0000

    Multi log - send and postpone icons
    git-svn-id: https://svn.code.sf.net/p/geokretylog/code@270 ff536b33-f402-454f-b109-af6ef765b36a

--- trunk/app/src/main/java/pl/nkg/geokrety/ui/MultiLogActivity.java
+++ trunk/app/src/main/java/pl/nkg/geokrety/ui/MultiLogActivity.java
@@ -1,7 +1,12 @@
 package pl.nkg.geokrety.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import pl.nkg.geokrety.R;
 
 public class MultiLogActivity extends AbstractActivity implements MultiLogFragment.OnFragmentInteractionListener {
 
@@ -19,5 +24,27 @@ public class MultiLogActivity extends AbstractActivity implements MultiLogFragme
         } else {
             mMultiLogFragment = (MultiLogFragment) getSupportFragmentManager().findFragmentById(android.R.id.content);
         }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_multilog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_postpone:
+                return true;
+
+            case R.id.action_send:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
